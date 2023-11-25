d73c7003145da8885aecd766be025599f01cf105
Mikhail Kozlov <gamestar595@gmail.com>
2023-11-25 22:03:46 +0300

    Changes

--- script.js
+++ script.js
@@ -6,21 +6,37 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     });
 
-    const selectSingle = document.querySelector('.select');
-    const selectSingle_title = selectSingle.querySelector('.select__title');
-    const selectSingle_text = selectSingle.querySelector('.select__title-text');
-    const selectSingle_labels = selectSingle.querySelectorAll('.select__label');
-    selectSingle_title.addEventListener('click', () => {
-        if ('active' === selectSingle.getAttribute('data-state')) {
-            selectSingle.setAttribute('data-state', '');
-        } else {
-            selectSingle.setAttribute('data-state', 'active');
-        }
-    });
-    for (let i = 0; i < selectSingle_labels.length; i++) {
-        selectSingle_labels[i].addEventListener('click', (e) => {
-            selectSingle_text.textContent = e.target.textContent;
-            selectSingle.setAttribute('data-state', '');
+    const selectSingle = document.querySelectorAll('.select');
+    if (selectSingle) {
+        selectSingle.forEach(element => {
+            const selectSingle_title = element.querySelector('.select__title');
+            const selectSingle_text = element.querySelector('.select__title-text');
+            const selectSingle_labels = element.querySelectorAll('.select__label');
+            selectSingle_title.addEventListener('click', () => {
+                if ('active' === element.getAttribute('data-state')) {
+                    element.setAttribute('data-state', '');
+                } else {
+                    element.setAttribute('data-state', 'active');
+                }
+            });
+            for (let i = 0; i < selectSingle_labels.length; i++) {
+                selectSingle_labels[i].addEventListener('click', (e) => {
+                    selectSingle_text.textContent = e.target.textContent;
+                    element.setAttribute('data-state', '');
+                });
+            }
         });
+    }
+
+    const tableItems = document.querySelectorAll('.table-item');
+    if (tableItems) {
+        tableItems.forEach(element => {
+            element.addEventListener('click', function () {
+                const option = element.querySelector('.schedule-option');
+                if (option) {
+                    option.classList.toggle('schedule-option-active');
+                }
+            })
+        })
     }
 });
